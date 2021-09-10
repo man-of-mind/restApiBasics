f20a3d1c8c155fed434f27a325c6cb893b59cb25
Abiodun Ganiyu <timothyabiodun300@gmail.com>
2021-09-10 21:55:27 +0100

    function based apiView

--- apibasics/serializers.py
+++ apibasics/serializers.py
@@ -7,4 +7,5 @@ from .models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ['id', 'title', 'author', 'email']
+#        fields = ['id', 'title', 'author', 'email']
+        fields = '__all__'
